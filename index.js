a295e93e8a2fab2f12f3243520ac52d40252c697
Alexander Mortensen <alextmortensen@gmail.com>
2017-10-18 19:01:04 -0600

    Adds routes and backend packages.

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+require('dotenv').config()
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/front_end/build'))
+
+app.post('/api/sign-in')
+app.post('/api/sign-up')
+
+app.get('/api/user/:id')
+app.put('/api/user/:id')
+app.delete('/api/user/:id')
+
+app.listen(process.env.PORT, () => {
+	console.log('Listening on port ' + process.env.PORT)
+})
